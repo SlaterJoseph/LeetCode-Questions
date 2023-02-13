14206b11c0c581540b33d1633f8a99ff058a8f0c
Joseph Slater <73854806+SlaterJoseph@users.noreply.github.com>
2023-02-13 15:13:10 -0500

    Create CountOddNumbersInIntervalRange.py

---
+++ CountOddNumbersInIntervalRange.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        # Use arithmetic. Find the difference between the low and high number
+        # If the low number is even, it is the difference / 2
+        # If the low number is odd, it is the (difference / 2) + 1
+        
+        base = high - low  # Difference of high and low
+        base //= 2  # Floor divide by 2
+        return base + 1 if low % 2 == 1 or high % 2 == 1 else base  # Increment if either high or low are odd
